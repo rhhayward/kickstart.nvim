f0bf7d5323c875e9979e420575d53f7bd30c9321
Ryan Hayward <rhhayward@att.net>
2025-10-18 20:15:32 +0000

    split window, create terminal

--- init.lua
+++ init.lua
@@ -92,6 +92,9 @@ vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
 -- Diagnostic keymaps
 vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
 
+-- Open a terminal window with <leader>t
+vim.keymap.set('n', '<leader>t', ':split | terminal<CR>', { noremap = true, silent = true, desc = 'Open terminal' })
+
 -- Exit terminal mode in the builtin terminal with a shortcut that is a bit easier
 -- for people to discover. Otherwise, you normally need to press <C-\><C-n>, which
 -- is not what someone will guess without a bit more experience.
